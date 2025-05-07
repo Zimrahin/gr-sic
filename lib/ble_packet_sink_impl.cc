e862bc6190a3de46b87a671bec60626a2e2cf672
Zimrahin <diegobadillosj@gmail.com>
2025-05-07 15:52:48 +0200

    Packet detection from base address working

--- lib/ble_packet_sink_impl.cc
+++ lib/ble_packet_sink_impl.cc
@@ -28,18 +28,19 @@ ble_packet_sink_impl::ble_packet_sink_impl(uint32_t base_address, uint preamble_
     : gr::sync_block("ble_packet_sink",
                      gr::io_signature::make(1, 1, sizeof(input_type)),
                      gr::io_signature::make(0, 0, 0)), // No output
-      d_access_code(static_cast<uint64_t>(base_address)),
       d_mask((~uint64_t(0)) >> (64 - d_access_code_len)),
       d_threshold(preamble_threshold)
 {
-    d_access_code = reverse_bits(d_access_code);
+    d_access_code = generate_access_code(base_address);
 }
 
 // Destructor
 ble_packet_sink_impl::~ble_packet_sink_impl() {}
 
+// Slice float data into binary data
 uint8_t ble_packet_sink_impl::slice(float data_in) { return data_in > 0 ? 1 : 0; }
 
+// Reverse bits in an unsigned integer
 template <typename T>
 T ble_packet_sink_impl::reverse_bits(T unsigned_integer)
 {
@@ -56,6 +57,23 @@ T ble_packet_sink_impl::reverse_bits(T unsigned_integer)
     return reversed;
 }
 
+// Generate access code for packet detection
+uint64_t ble_packet_sink_impl::generate_access_code(uint32_t base_address)
+{
+    uint64_t access_code = 0;
+    uint8_t address_prefix = 0x00;
+    uint8_t preamble = (base_address & 0x01) ? 0xAA : 0x55;
+    base_address = reverse_bits(base_address);
+
+    // From LSB to MSB: 0x00 | base_address | preamble
+    access_code |= static_cast<uint64_t>(base_address) << 8 | address_prefix;
+    access_code |= static_cast<uint64_t>(preamble) << 40;
+
+    return access_code;
+}
+
+
+// Called for each chunk of data in the input stream
 int ble_packet_sink_impl::work(int noutput_items,
                                gr_vector_const_void_star& input_items,
                                gr_vector_void_star& output_items)
@@ -74,8 +92,6 @@ int ble_packet_sink_impl::work(int noutput_items,
         } else {
             wrong_bits = (d_shift_reg ^ d_access_code) & d_mask;
             volk_64u_popcnt(&nwrong, wrong_bits); // Count wrong bits
-
-            // std::cout << "nwrong=" << nwrong << std::endl;
         }
 
         // Shift in new data
--- lib/ble_packet_sink_impl.h
+++ lib/ble_packet_sink_impl.h
@@ -19,12 +19,13 @@ class ble_packet_sink_impl : public ble_packet_sink
 private:
     template <typename T>
     T reverse_bits(T unsigned_integer); // Reverse bits in an unsigned integer
-    uint8_t slice(float data_in);       // Slice float data into binary data
-    // uint64_t generate_access_code(uint32_t base_address); // Generate access code
+    uint64_t generate_access_code(uint32_t base_address); // Generate access code
+    uint8_t slice(float data_in); // Slice float data into binary data
 
-    uint d_access_code_len = 32; // Length of the code
+    uint d_access_code_len = 48; // 1B preamble + 4B base address + 1B address prefix
     uint64_t d_shift_reg = 0;    // Shift register for the incoming data
     uint d_fill_buffer = 0;      // Ensures the shift register is filled before comparing
+    uint32_t d_base_address;     // Base address for the access code
     uint64_t d_access_code;      // Access code to be detected
     uint64_t d_mask;             // (1 << code_len) - 1
     uint d_threshold;            // Allowed bit errors in the preamble detection
@@ -33,7 +34,7 @@ public:
     ble_packet_sink_impl(uint32_t base_address, uint preamble_threshold);
     ~ble_packet_sink_impl();
 
-    // Where all the action really happens
+    // Called for each chunk of data in the input stream
     int work(int noutput_items,
              gr_vector_const_void_star& input_items,
              gr_vector_void_star& output_items);
