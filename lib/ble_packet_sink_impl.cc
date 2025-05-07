5ee915a5123419422cd18fca513ab3a487b21f88
Zimrahin <diegobadillosj@gmail.com>
2025-05-07 19:33:35 +0200

    Refactor: Add FSM, define variables in constructor, declare public first

--- lib/ble_packet_sink_impl.cc
+++ lib/ble_packet_sink_impl.cc
@@ -30,11 +30,18 @@ ble_packet_sink_impl::ble_packet_sink_impl(uint32_t base_address,
     : gr::sync_block("ble_packet_sink",
                      gr::io_signature::make(1, 1, sizeof(input_type)),
                      gr::io_signature::make(0, 0, 0)), // No output
-      d_mask((~uint64_t(0)) >> (64 - d_access_code_len)),
       d_threshold(preamble_threshold),
-      d_lfsr(lfsr)
+      d_lfsr_default(lfsr)
 {
+    // Constants
+    d_access_code_len = 48; // 1B preamble + 4B base address + 1B address prefix
+    d_mask = (~uint64_t(0)) >> (64 - d_access_code_len);
     d_access_code = generate_access_code(base_address);
+
+    // Variables
+    d_shift_reg = 0;
+    d_fill_buffer = 0;
+    d_state = state::SEARCH_PREAMBLE;
 }
 
 // Destructor
@@ -89,6 +96,41 @@ uint8_t ble_packet_sink_impl::whiten_bit(uint8_t data_bit, uint8_t polynomial = 
     return whitened_bit;
 }
 
+// Finite State Machine methods
+void ble_packet_sink_impl::enter_search_preamble()
+{
+    d_state = state::SEARCH_PREAMBLE;
+    d_shift_reg = 0;
+    d_fill_buffer = 0;
+}
+void ble_packet_sink_impl::enter_decode_length()
+{
+    d_state = state::DECODE_LENGTH;
+    d_lfsr = d_lfsr_default; // Reset LFSR for whitening
+}
+void ble_packet_sink_impl::process_search_preamble(uint8_t bit, uint64_t sample_index)
+{
+    // Buffer yet to be filled
+    if (d_fill_buffer < d_access_code_len) {
+        d_fill_buffer++;
+        return;
+    }
+    // Count wrong bits
+    uint64_t diff = (d_shift_reg ^ d_access_code) & d_mask;
+    uint64_t nwrong = d_threshold + 1; // Value greater than the threshold
+    volk_64u_popcnt(&nwrong, diff);
+
+    if (nwrong <= d_threshold) {
+        std::cout << "Preamble found at sample " << sample_index << "\n";
+        enter_decode_length();
+    }
+}
+void ble_packet_sink_impl::process_decode_length(uint8_t bit, uint64_t sample_index)
+{
+    // TODO: implement length decoding
+}
+
+
 // Called for each chunk of data in the input stream
 int ble_packet_sink_impl::work(int noutput_items,
                                gr_vector_const_void_star& input_items,
@@ -99,24 +141,19 @@ int ble_packet_sink_impl::work(int noutput_items,
     uint64_t sample_count = nitems_read(0);
 
     for (int i = 0; i < noutput_items; i++) {
-        // Compute hamming distance between desired access code and current data
-        uint64_t wrong_bits = 0;
-        uint64_t nwrong = d_threshold + 1; // Value greater than the threshold
+        uint8_t rx_bit = slice(in[i]);
+        d_shift_reg = (d_shift_reg << 1) | (rx_bit & 1); // Shift in new bit
 
-        if (d_fill_buffer < d_access_code_len) { // Buffer yet to be filled
-            d_fill_buffer++;
-        } else {
-            wrong_bits = (d_shift_reg ^ d_access_code) & d_mask;
-            volk_64u_popcnt(&nwrong, wrong_bits); // Count wrong bits
-        }
+        switch (d_state) {
+        case state::SEARCH_PREAMBLE:
+            process_search_preamble(rx_bit, sample_count + i);
+            break;
 
-        // Shift in new data
-        d_shift_reg = (d_shift_reg << 1) | (slice(in[i]) & 1);
-        if (nwrong <= d_threshold) {
-            std::cout << "Detected access code at " << sample_count + i << std::endl;
+        case state::DECODE_LENGTH:
+            process_decode_length(rx_bit, sample_count + i);
+            break;
         }
     }
-
     return noutput_items;
 }
 
--- lib/ble_packet_sink_impl.h
+++ lib/ble_packet_sink_impl.h
@@ -16,21 +16,6 @@ namespace ble {
 
 class ble_packet_sink_impl : public ble_packet_sink
 {
-private:
-    template <typename T>
-    T reverse_bits(T unsigned_integer); // Reverse bits in an unsigned integer
-    uint64_t generate_access_code(uint32_t base_address); // Generate access code
-    uint8_t slice(float data_in); // Slice float data into binary data
-
-    uint d_access_code_len = 48; // 1B preamble + 4B base address + 1B address prefix
-    uint64_t d_shift_reg = 0;    // Shift register for the incoming data
-    uint d_fill_buffer = 0;      // Ensures the shift register is filled before comparing
-    uint32_t d_base_address;     // Base address for the access code
-    uint64_t d_access_code;      // Access code to be detected
-    uint64_t d_mask;             // (1 << code_len) - 1
-    uint d_threshold;            // Allowed bit errors in the preamble detection
-    uint8_t d_lfsr;              // Linear Feedback Shift Register for whitening
-
 public:
     ble_packet_sink_impl(uint32_t base_address, uint preamble_threshold, uint8_t lfsr);
     ~ble_packet_sink_impl();
@@ -41,6 +26,34 @@ public:
     int work(int noutput_items,
              gr_vector_const_void_star& input_items,
              gr_vector_void_star& output_items);
+
+private:
+    // Finite State Machine
+    enum class state { SEARCH_PREAMBLE, DECODE_LENGTH };
+    state d_state;
+    void enter_search_preamble();
+    void enter_decode_length();
+    void process_search_preamble(uint8_t bit, uint64_t sample_index);
+    void process_decode_length(uint8_t bit, uint64_t sample_index);
+
+    // Helper functions
+    template <typename T>
+    T reverse_bits(T unsigned_integer); // Reverse bits in an unsigned integer
+    uint64_t generate_access_code(uint32_t base_address); // Generate access code
+    uint8_t slice(float data_in); // Slice float data into binary data
+
+    // Constants
+    uint d_threshold;        // Allowed bit errors in the preamble detection
+    uint d_access_code_len;  // 1B preamble + 4B base address + 1B address prefix
+    uint32_t d_base_address; // Base address for the access code
+    uint64_t d_access_code;  // Access code to be detected
+    uint64_t d_mask;         // (1 << code_len) - 1
+    uint8_t d_lfsr_default;  // Linear Feedback Shift Register for whitening
+
+    // Variables
+    uint64_t d_shift_reg; // Shift register for the incoming data
+    uint d_fill_buffer;   // Ensures the shift register is filled before comparing
+    uint8_t d_lfsr;       // Linear Feedback Shift Register for whitening
 };
 
 } // namespace ble
