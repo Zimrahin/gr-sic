552436dd75d942ff203171fd2b3f850295369bcc
Zimrahin <diegobadillosj@gmail.com>
2025-07-10 16:30:52 +0200

    IEEE 802.15.4 example: Increase throttle sample rate

--- examples/ieee802154_packet_example.py
+++ examples/ieee802154_packet_example.py
@@ -214,7 +214,7 @@ class ieee802154_packet_example(gr.top_block, Qt.QWidget):
             128,
             [])
         self.blocks_uchar_to_float_0_0_0 = blocks.uchar_to_float()
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, (samp_rate/100), True, 0 if "auto" == "auto" else max( int(float(0.1) * (samp_rate/100)) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, (samp_rate/2), True, 0 if "auto" == "auto" else max( int(float(0.1) * (samp_rate/2)) if "auto" == "time" else int(0.1), 1) )
         self.blocks_sub_xx_0 = blocks.sub_ff(1)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/diego/Documents/GNU_radio_OOT_modules/gr-sic/examples/data/802154_84B.dat', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
@@ -288,7 +288,7 @@ class ieee802154_packet_example(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_quadrature_demod_cf_0.set_gain(((self.samp_rate / self.decimation)/(2*math.pi*self.fsk_deviation_hz)))
-        self.blocks_throttle2_0.set_sample_rate((self.samp_rate/100))
+        self.blocks_throttle2_0.set_sample_rate((self.samp_rate/2))
         self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, (self.tuning_LPF_cutoff_kHz*1000), (self.samp_rate/100), window.WIN_HAMMING, 6.76))
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_time_sink_x_1.set_samp_rate(int(self.samp_rate / self.samples_per_chip))
